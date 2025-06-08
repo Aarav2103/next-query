6c35cc7be1267b6c81447f5f5ad2c61d3317ded2
Aarav <aarav00021@gmail.com>
2025-06-09 00:41:14 +0530

    - Create configs and collections for answers, comments, questions, and votes - Set up middleware - Add Zustand for state management

--- src/models/name.ts
+++ src/models/name.ts
@@ -1,1 +1,8 @@
  
+
+export const db = "maindb"
+export const questionCollection = "questions"
+export const answerCollection = "answers"
+export const commentCollection = "comments"
+export const voteCollection = "votes"
+export const questionAttachmentBucket = "question-attachment"
